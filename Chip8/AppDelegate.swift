073681c42f01362cbb034ecde5a4903a868fab2a
Alex Scown <scowny2000@yahoo.co.uk>
2018-05-25 18:59:36 +0100

    Initial Commit

---
+++ Chip8/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Chip8
+//
+//  Created by Alexander Scown on 25/5/18.
+//  Copyright © 2018 Alex Scown. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Chip8/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Chip8
+//
+//  Created by Alexander Scown on 25/5/18.
+//  Copyright © 2018 Alex Scown. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
