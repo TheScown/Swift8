1a3adecc079f1ac19282f17231bcf65b0d31329e
Alex Scown <scowny2000@yahoo.co.uk>
2018-07-21 16:41:57 +0100

    Add stack to UI

--- Chip8/Stack.swift
+++ Chip8/Stack.swift
@@ -10,25 +10,51 @@ import Cocoa
 
 class Stack: NSObject {
 
-    private var stack = [UInt16]()
-    private var SP = -1
+    @objc dynamic var stack = [AddressCell]()
+    var SP = -1
+    @objc dynamic var indexSet: IndexSet = IndexSet()
     
     func reset() {
-        stack = Array(repeating: 0, count: 16)
+        stack = (0 ..< 16).map {(index) in AddressCell(index, 0)}
         SP = -1
+        indexSet = IndexSet.init(integer: -1)
     }
     
     func push(_ x: UInt16) {
-        SP += 1
-        stack[Int(SP)] = x
+        DispatchQueue.main.sync {
+            indexSet.remove(SP)
+            SP += 1
+            indexSet.insert(SP)
+            stack[SP] = AddressCell(SP, x)
+        }
     }
     
     func pop() -> UInt16 {
-        let result = stack[Int(SP)]
+        var finalResult: UInt16 = 0
         
-        SP -= 1
+        DispatchQueue.main.sync {
+            let result = stack[SP]
+            stack[SP] = AddressCell(SP, 0)
+            indexSet.remove(SP)
+            
+            SP -= 1
+            
+            indexSet.insert(SP)
+            
+            finalResult = result.value
+        }
         
-        return result
+        return finalResult
     }
     
+}
+
+class AddressCell: NSObject {
+    @objc dynamic let address: Int
+    @objc dynamic let value: UInt16
+    
+    init(_ address: Int, _ value: UInt16) {
+        self.address = address
+        self.value = value
+    }
 }
